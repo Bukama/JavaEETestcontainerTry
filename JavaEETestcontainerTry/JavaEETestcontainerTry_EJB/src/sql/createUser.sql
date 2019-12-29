6f91daa8795f6b6c18dc9a90ec2e6332a2187eff
bish <hardcore_army@web.de>
2019-12-29 10:44:42 +0100

    Transfer commit

--- JavaEETestcontainerTry/JavaEETestcontainerTry_EJB/src/sql/createUser.sql
+++ JavaEETestcontainerTry/JavaEETestcontainerTry_EJB/src/sql/createUser.sql
@@ -1,15 +1,30 @@
+-- First step: Create schemauser
+
+CREATE USER schemauser
+  IDENTIFIED BY oracle;
+
+GRANT CONNECT TO schemauser;
+GRANT create session TO schemauser;
+GRANT create table TO schemauser;
+GRANT create view TO schemauser;
+GRANT create any trigger TO schemauser;
+GRANT create any procedure TO schemauser;
+GRANT create sequence TO schemauser;
+GRANT create synonym TO schemauser;
+
+
+-- Second step: Create user for application / test data and grant them rights
+
 CREATE USER readinguser IDENTIFIED BY oracle;
 
 GRANT CONNECT TO readinguser;
 
-GRANT SELECT, INSERT, UPDATE on bish.emp TO readinguser;
+GRANT SELECT, INSERT, UPDATE on schemauser.emp TO readinguser;
 
 
 
-CREATE USER writingguser IDENTIFIED BY oracle;
+CREATE USER writinguser IDENTIFIED BY oracle;
 
-GRANT CONNECT TO writingguser;
+GRANT CONNECT TO writinguser;
 
-GRANT SELECT, INSERT, UPDATE, DELETE on bish.emp TO writingguser;
-
-GRANT CREATE ANY TABLE to writingguser;
+GRANT SELECT, INSERT, UPDATE, DELETE on schemauser.emp TO writinguser;
