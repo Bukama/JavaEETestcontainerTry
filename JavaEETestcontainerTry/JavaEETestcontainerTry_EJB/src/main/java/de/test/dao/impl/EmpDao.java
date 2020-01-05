f625e71503f55fd752e89f997d403fafb337ffe8
Bukama <hardcore_army@web.de>
2020-01-05 17:47:58 +0100

    Testing writingDataSource

--- JavaEETestcontainerTry/JavaEETestcontainerTry_EJB/src/main/java/de/test/dao/impl/EmpDao.java
+++ JavaEETestcontainerTry/JavaEETestcontainerTry_EJB/src/main/java/de/test/dao/impl/EmpDao.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class EmpDao implements IEmpDao {
 
     @PersistenceContext(unitName = "ReadingDS")
+    // @PersistenceContext(unitName = "WritingDS")
     private EntityManager em;
 
     public void removeAllEmps() {
